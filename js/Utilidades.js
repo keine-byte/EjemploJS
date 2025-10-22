99ebf6c375374c3948e86176777f8b3b0df3a217
Keiner Elias Beltran <keiner.beltran@cecar.edu.co>
2025-10-22 08:12:21 -0500

    Ejemplos JS

---
+++ js/Utilidades.js
@@ -0,0 +1,46 @@
+function crearElemento(tag) {
+
+    return document.createElement(tag);
+}
+
+function adicionarTextoATag(tag, texto) {
+
+    tag.innerHTML = texto;
+
+}
+
+
+
+function adicionarTextoAContenedor(tag, contenedor) {
+
+    contenedor.appendChild(tag);
+
+
+
+}
+
+function adicionarTagABody(tag) {
+
+
+    document.body.appendChild(tag);
+
+
+
+}
+
+function crearTagConTexto(tag, texto) {
+
+    var tag = crearElemento(tag);
+
+    adicionarTextoATag(tag, texto);
+
+    return tag;
+
+}
+
+function crearTagConTextoAdicionarABody(tag, texto) {
+
+    var tag = crearTagConTexto(tag, texto);
+    adicionarTagABody(tag);
+
+}
